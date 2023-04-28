426e5436b6c63394363624705064ce7e294ab553
Daniil.Musin <Daniil.Musin@urfu.me>
2023-04-28 20:00:42 +0500

    task 3, Variant 20

--- PhotoEnhancer/PhotoEnhancer/Program.cs
+++ PhotoEnhancer/PhotoEnhancer/Program.cs
@@ -21,10 +21,23 @@ namespace PhotoEnhancer
 
             var mainForm = new MainForm();
 
+
+            mainForm.AddFilter(new PixelFilter<RangeDecreasingParameters>(
+                "Сужение диапазона",
+                (pixel, parameters) =>
+                {
+                    var l1 = parameters.LowerLevel;
+                    var l2 = parameters.UpperLevel;
+                    return new Pixel(
+                        pixel.R * (l2 - l1) + l1,
+                        pixel.G * (l2 - l1) + l1,
+                        pixel.B * (l2 - l1) + l1
+                    );
+                }));
             mainForm.AddFilter(new PixelFilter<LighteningParameters>(
                 "Осветление/затемнение",
                 (pixel, parameters) => pixel * parameters.Coefficient));
-            
+
             mainForm.AddFilter(new PixelFilter<EmptyParameters>(
                 "Оттенки серого",
                 (pixel, parameters) =>
@@ -32,6 +45,27 @@ namespace PhotoEnhancer
                     var lightness = 0.3 * pixel.R + 0.6 * pixel.G + 0.1 * pixel.B;
                     return new Pixel(lightness, lightness, lightness);
                 }));
+            mainForm.AddFilter(new TransformFilter(
+                "Квадратура",
+                (size) =>
+                {
+                    var quadra = Math.Min(size.Height, size.Width);
+                    return new Size(quadra, quadra);
+                },
+                (point, size) =>
+                {
+                    if (size.Width >= size.Height)
+                    {
+                        double ratio = (double)size.Width / (double)size.Height;
+                        return new Point(Convert.ToInt32((double)point.X * ratio), point.Y);
+                    }
+                    else
+                    {
+                        double ratio = (double)size.Height / (double)size.Width;
+                        return new Point(point.X, Convert.ToInt32((double)point.X / ratio));
+                    }
+                }
+                ));
 
             mainForm.AddFilter(new TransformFilter(
                 "Отражение по горизонтали",
